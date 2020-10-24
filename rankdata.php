ed253a43b3c5c00abfbb17542bfaedc2ba295135
kilroy-2 <45923163+kilroy-2@users.noreply.github.com>
2020-10-24 03:08:39 +0200

    Update rankdata.php

--- rankdata.php
+++ rankdata.php
@@ -8,6 +8,14 @@
 	$user = ''; // default
 	$plat = 'steam'; // default
 
+	if (!empty($_GET['user'])) { // is user parameter given 
+		$user = str_replace(array(' ', '%20'), array('-', '-') , strtolower($_GET['user'])); // set user the value of given parameter in lower case and replace spaces with hyphen 
+	}
+	if (!empty($_GET['platform'])) { // is plat parameter given
+		$plat = strtolower($_GET['platform']); // set plat the value of given parameter in lower case
+	}
+
+
 	$rankData = array(
 				'message' => 'Success',
 				'code' => '1',
@@ -33,28 +41,10 @@
 				'MVPs' => 0,
 				'GoalShotRatio' => 0.0
 	);
-	
-	function hasPlaylist($name, $data){
-		for ($i = 0 ; $i < count($data) ; $i++){
-			if (isset($data[$i]['metadata'])){
-				if ($data[$i]['metadata']['name'] == $name){
-					return $i;
-				}
-			}
-		}
-		return -1;
-	}
-
-	if (!empty($_GET['user'])) { // is user parameter given 
-		$user = str_replace(array(' ', '%20'), array('-', '-') , strtolower($_GET['user'])); // set user the value of given parameter in lower case and replace spaces with hyphen 
-	}
-	if (!empty($_GET['platform'])) { // is plat parameter given
-		$plat = strtolower($_GET['platform']); // set plat the value of given parameter in lower case
-	}
 
 
 	// valid platforms
-	$platforms = array('psn'=>1, 'steam'=>1, 'xbl'=>1); 
+	$platforms = array('psn'=>1, 'steam'=>1, 'xbox'=>1); 
 
 
 	// no username or too long username
@@ -73,47 +63,55 @@
 
 
 
-	$RL_tracker = @file_get_contents('https://rocketleague.tracker.network/rocket-league/profile/'.$plat.'/'.$user.'/overview'); // get html code
+	$RL_tracker = @file_get_contents('https://api.tracker.gg/api/v2/rocket-league/standard/profile/'.$plat.'/'.$user.'/'); // get html code
 
-	preg_match_all("/\"segments\":(.+?),\"availableSegments\"/is", $RL_tracker, $first); // first = json of stats and rank data
-	preg_match_all("/\"platformUserHandle\":\"(.+?)\",\"platformUserIdentifier\"/is", $RL_tracker, $name); // fetch name
-
-	$rankData['name'] = isset($name[1][0]) ? html_entity_decode($name[1][0], ENT_QUOTES | ENT_XML1, 'UTF-8') : $user; // set name
+	$data = json_decode($RL_tracker, true); // decode to php array 
 
 
-	if (count($first[0])==0) { // checking for existing data (if not, no ranks given)
+	if($data === NULL){
 		$rankData['message'] = $user.' ('.$plat.') has no data on rocketleague.tracker.network yet.';
 		$rankData['code'] = '0';
 		die(json_encode($rankData));
 	}
 
-	$data = json_decode($first[1][0], true); // decode to php array 
-	// or use the array $data instead of $rankData, ofc it has lots more info.
-	// var_dump($data); 
 
-	$rankData['SeasonReward'] = array($data[0]['stats']['seasonRewardLevel']['value'], $data[0]['stats']['seasonRewardWins']['value']);
-	$rankData['Wins'] = $data[0]['stats']['wins']['value'];
-	$rankData['Goals'] = $data[0]['stats']['goals']['value'];
-	$rankData['MVPs'] = $data[0]['stats']['mVPs']['value'];
-	$rankData['Saves'] = $data[0]['stats']['saves']['value'];
-	$rankData['Assists'] = $data[0]['stats']['assists']['value'];
-	$rankData['Shots'] = $data[0]['stats']['shots']['value'];
-	$rankData['GoalShotRatio'] = $data[0]['stats']['goalShotRatio']['value'];
+	$rankData['name'] = html_entity_decode($data['data']['platformInfo']['platformUserHandle'], ENT_QUOTES | ENT_XML1, 'UTF-8');
+
+	// decode to php array 
+	// or use the array $data instead of $rankData, ofc it has lots more info.
+	// var_dump($data['data']); 
+
+	$rankData['SeasonReward'] = array($data['data']['segments'][0]['stats']['seasonRewardLevel']['value'], $data['data']['segments'][0]['stats']['seasonRewardWins']['value']);
+	$rankData['Wins'] = $data['data']['segments'][0]['stats']['wins']['value'];
+	$rankData['Goals'] = $data['data']['segments'][0]['stats']['goals']['value'];
+	$rankData['MVPs'] = $data['data']['segments'][0]['stats']['mVPs']['value'];
+	$rankData['Saves'] = $data['data']['segments'][0]['stats']['saves']['value'];
+	$rankData['Assists'] = $data['data']['segments'][0]['stats']['assists']['value'];
+	$rankData['Shots'] = $data['data']['segments'][0]['stats']['shots']['value'];
+	$rankData['GoalShotRatio'] = $data['data']['segments'][0]['stats']['goalShotRatio']['value'];
 
 	$playlistNames = array('Ranked Duel 1v1'=>'1v1', 'Ranked Doubles 2v2'=>'2v2', 'Ranked Solo Standard 3v3'=>'Solo 3v3', 'Ranked Standard 3v3'=>'3v3', 'Hoops' => 'Hoops', 'Rumble' => 'Rumble', 'Dropshot' => 'Dropshot', 'Snowday' => 'Snowday', 'Tournament Matches' => 'Tournament'); // array of short formatted playlist names
 
-	if(count($data) > 1 ) { // not sure if consistent yet, maybe needs some revamp
-		foreach ($playlistNames as $key => $value) {
-			$id = hasPlaylist($key, $data);
-
-			if($id != -1){
-				$rankData[$value] = array($data[$id]['stats']['tier']['value'], $data[$id]['stats']['rating']['value']); // rankNumber, MMR
+	function hasPlaylist($name, $data){
+		for ($i = 0 ; $i < count($data) ; $i++){
+			if (isset($data[$i]['metadata'])){
+				if ($data[$i]['metadata']['name'] == $name){
+					return $i;
+				}
 			}
+		}
+		return -1;
+	}
+
+	foreach ($playlistNames as $key => $value) {
+		$id = hasPlaylist($key, $data['data']['segments']);
+
+		if($id != -1){
+			$rankData[$value] = array($data['data']['segments'][$id]['stats']['tier']['value'], $data['data']['segments'][$id]['stats']['rating']['value']); // rankNumber, MMR
 		}
 	}
 
-		
-	
+
 	$rewardLevels = array('Unranked', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Champion', 'Grand Champion', 'Supersonic Legend'); // array of all possible reward levels (bottom up)
 
 	$rankNames = array('Unranked', 'Bronze I', 'Bronze II', 'Bronze III', 'Silver I', 'Silver II', 'Silver III', 'Gold I', 'Gold II', 'Gold III', 'Platinum I', 'Platinum II', 'Platinum III', 'Diamond I', 'Diamond II', 'Diamond III', 'Champion I', 'Champion II', 'Champion III', 'Grand Champion I', 'Grand Champion II', 'Grand Champion III', 'Supersonic Legend'); // array of all possible rank names (bottom up)
